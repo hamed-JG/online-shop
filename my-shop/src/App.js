92e2c605d2b1cec7d75626e0d56238d87db6f652
Hamed Jahan <iamhamedjahan@gmail.com>
2022-06-04 22:23:47 +0430

    create phone cards

--- my-shop/src/App.js
+++ my-shop/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Cards from "./components/Cards";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner/> 
+      <Cards/>
     </div>
   );
 }
---
+++ my-shop/src/components/Cards.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import styles from "./Cards.module.css";
+import iphone13 from "../images/iphone 13.jpg";
+import samsungS21 from "../images/samsung S21.webp";
+import xiaomi from "../images/xiaomi mi11.jpg";
+import huawei from "../images/Huawei P30.jpg";
+import Card from "./Card.js";
+class Cards extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Card image={iphone13} name='Iphone13' cost='1000$' />
+        <Card image={samsungS21} name='Samsung S21' cost='1500$' />
+        <Card image={xiaomi} name='XiaomiMi11' cost='800$' />
+        <Card image={huawei} name='iphone13' cost='1000$' />
+      </div>
+    );
+  }
+}
+
+export default Cards;
